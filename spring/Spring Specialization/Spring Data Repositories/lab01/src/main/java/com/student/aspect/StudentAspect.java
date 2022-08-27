4a84d3c1d491dd2f1bd934e7ee715c19af55b0d0
Charles Stockman <chuck.stockman@yahoo.com>
2022-08-27 01:20:31 -0400

    Add comments to StudentAspect.java

--- spring/Spring Specialization/Spring Data Repositories/lab01/src/main/java/com/student/aspect/StudentAspect.java
+++ spring/Spring Specialization/Spring Data Repositories/lab01/src/main/java/com/student/aspect/StudentAspect.java
@@ -8,23 +8,38 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.hibernate.annotations.common.util.impl.LoggerFactory;
-import org.slf4j.Logger;
 
+/**
+ * The purpose is to put all the logging into one place.
+ */
 @Named
 @Aspect
 public class StudentAspect {
    private org.jboss.logging.Logger LOG = LoggerFactory.logger(this.getClass());
 
+   /**
+    * Query to find where the code will be executed.
+    */
    @Pointcut("execution( * com.student.service.*.*(..))")
    public void log() {
     
    }
 
+   /**
+    * Code that will be executed before the point cut.
+    *
+    * @param joinPoint    Information about the point where the code will be executed
+    */
    @Before("log()")
-   public void before(JoinPoint joinPont) {
-      LOG.info("Invoked Method Before -> " + joinPont.getSignature().getName());
+   public void before(JoinPoint joinPoint) {
+      LOG.info("Invoked Method Before -> " + joinPoint.getSignature().getName());
    }
 
+   /**
+    * Code that will be executed after the point cut.
+    *
+    * @param joinPoint    Information about the point where the code will be executed
+    */
    @After("log()")
    public void after(JoinPoint joinPoint) {
       LOG.info("Invoked Method after -> " + joinPoint);
