3a8ddfabfb5750f5347c9e68172e7caacd6a94b0
Charles Stockman <charlesstockman@Charless-iMac.lan>
2024-03-03 22:01:49 -0500

    added code since I am rebuilding the os, don't know stae of code

---
+++ spring/packt-spgirngboot-3/microservices/recommendation-service/src/main/java/se/magnus/microservices/core/recommendation/services/RecommendationServiceImplementation.java
@@ -0,0 +1,7 @@
+package se.magnus.microservices.core.recommendation.services;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationServiceImplementation {
+}
---
+++ spring/packt-spgirngboot-3/microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImplementation.java
@@ -0,0 +1,7 @@
+package se.magnus.microservices.core.review.services;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewServiceImplementation {
+}
