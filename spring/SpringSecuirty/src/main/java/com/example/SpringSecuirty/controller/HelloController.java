b43045e389d8103ec808ff036fd8c0764897faaa
CharlesStockman <chuck.stockman@yahoo.com>
2024-08-26 14:28:36 -0400

    Discussed Archtitecture and name/password login
    Created own password through the application.properties

--- spring/SpringSecuirty/src/main/java/com/example/SpringSecuirty/controller/HelloController.java
+++ spring/SpringSecuirty/src/main/java/com/example/SpringSecuirty/controller/HelloController.java
@@ -1,13 +1,30 @@
 package com.example.SpringSecuirty.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Behind the scene, every controller gets converted to a servet (tomcat is a servlet container).
+// Front End Controller is the dispatcher servlet.
+// Goes from the Filter Chain to the Front End Controller to the Controller.
+//
+// Filter Chain -- Spring Security adds its own filters examples below (Chain of Command Pattern)
+//      SecurityContextPersistenceFilter LogoutFilter UserNamePasswordAuthenticateFilter DefaultLoginPageGeneratingFilter,
+//      RequestCacheAwareFilter etc.
+// Can customize filters
+// Can change the request/response
+//
+// Session
+//    For request the session id (JSESSIONID) is part of the cookie
+//
+// Can get the username/password from the application.properties
+// Postman : Authorization ( Type: Basic Auth )
 @RestController
 public class HelloController {
 
+
     @GetMapping("/")
-    public String greet() {
-        return "Hello Secure World";
+    public String greet(HttpServletRequest request) {
+        return "Hello Secure World for session id " + request.getSession().getId();
     }
 }
