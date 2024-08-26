86f0da34ce923e7afb896526f049a871b9009e6f
CharlesStockman <chuck.stockman@yahoo.com>
2024-08-26 13:10:20 -0400

    Create Spring Project used default login screen

---
+++ spring/SpringSecuirty/src/test/java/com/example/SpringSecuirty/SpringSecuirtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecuirty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuirtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
