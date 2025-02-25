4ade8825f4f0b8e5cb2f6b3fd45845a0cdb838f3
CharlesStockman <chuck.stockman@yahoo.com>
2025-02-24 21:48:40 -0500

    Demo now runs as Default SpringBoot App

---
+++ spring/spring-aop/src/main/java/com/example/springAop/SpringAopExample.java
@@ -0,0 +1,11 @@
+package com.example.springAop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopExample {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopExample.class, args);
+    }
+}
--- spring/spring-aop/src/test/java/com/example/spring6RestMvc/SpringAopExample.java
+++ spring/spring-aop/src/test/java/com/example/spring6RestMvc/SpringAopExample.java
@@ -1,4 +1,4 @@
-package com.example.spring6RestMvc;
+package com.example.springAop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
